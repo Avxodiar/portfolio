2a8f4f26c873be6405b9ea09877685681cae3440
KHaritov Andrei <avxodiar@gmail.com>
2020-12-19 23:11:20 +0300

    Визуальное оформление и доработано плавное перемещение для ссылок не из основного меню

--- maket/js/script.js
+++ maket/js/script.js
@@ -3,27 +3,61 @@
 ( function() {
 
 /* плавная прокрутка с отступом сверху для фикс.шапки */
-const topOffset = document.querySelector('nav.main').offsetHeight + 12;
-document.querySelectorAll('nav.main a[href^="#"').forEach(link => {
 
-    link.addEventListener('click', function(evt) {
-        evt.preventDefault();
+const navMenu = document.querySelector('nav.main');
 
-        let href = this.getAttribute('href').substring(1);
+// отступ
+const topOffset = navMenu.offsetHeight + 12;
 
-        document.querySelector('nav.main .active').classList.remove("active");
-        const currentMenu = this.parentElement;
-        currentMenu.classList.add("active");
-
-        const scrollTarget = document.getElementById(href);
-        const elementPosition = scrollTarget.getBoundingClientRect().top;
-        const offsetPosition = elementPosition - topOffset;
-
-        window.scrollBy({
-            top: offsetPosition,
-            behavior: 'smooth'
-        });
-    });
+// основное меню
+navMenu.querySelectorAll('a[href^="#"').forEach(link => {
+   link.addEventListener('click', onClickAnchor);
 });
+
+// внутренние ссылки на якоря
+document.querySelectorAll('.to-anchor').forEach(link => {
+   link.addEventListener('click', onClickAnchor);
+});
+
+
+/**
+ * Плавная прокрутка к секцци по ее якорю
+ */
+function onClickAnchor(evt)  {
+    evt.preventDefault();
+
+    let href = this.getAttribute('href').substring(1);
+
+    const scrollTarget = document.getElementById(href);
+
+    setActiveMenu(this, scrollTarget);
+
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+        top: offsetPosition,
+        behavior: 'smooth'
+    });
+}
+
+/**
+ * Установка нового активного пункта меню
+ */
+function setActiveMenu(elem, target)
+{
+    let currentMenu = navMenu.querySelector('.active');
+    if (currentMenu){
+        currentMenu.classList.remove("active");
+    }
+
+    let targetMenu;
+    if (elem.classList.contains('to-anchor')) {
+        targetMenu = navMenu.querySelector('a[href^="#'+target.id+'"');
+    } else {
+        targetMenu = elem.parentElement;
+    }
+    targetMenu.classList.add("active");
+}
 
 })();
